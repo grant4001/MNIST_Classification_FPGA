4cf9e56d7425ec1c749d2561925d848029a7feee
Grant Yu <grantyu2020@u.northwestern.edu>
2020-01-20 16:48:14 -0600

    licensing added

--- Project_FPGA/ff_line_buffer_groups.sv
+++ Project_FPGA/ff_line_buffer_groups.sv
@@ -1,3 +1,20 @@
+/*
+Copyright 2019, Grant Yu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Module: ff_line_buffer_groups.sv
 // Date: 12/13/2019
 // Description: FF_LINE_BUFFER_GROUPS contains all groups of line buffers which are
